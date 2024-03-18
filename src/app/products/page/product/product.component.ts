5f0783dd70cdf58693353540202719e96774723b
Elena Arocena <mariaelena.arocena@gmail.com>
2024-03-18 16:13:47 +0100

    feat(product): add form example and create directive

--- src/app/products/page/product/product.component.ts
+++ src/app/products/page/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
+    // constructor( private fb: FormBuilder ) {}
+    private fb = inject( FormBuilder );
+
+    public color:string = 'green';
+
+    public myForm: FormGroup = this.fb.group({
+      name: ['', [ Validators.required, Validators.minLength(6), Validators.email ] ]
+    });
+
+
+    changeColor() {
+      this.color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    }
 }
